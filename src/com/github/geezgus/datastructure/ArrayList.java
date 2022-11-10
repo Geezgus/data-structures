1d76d1c87563fa1a72765a9611cc61ebfd531403
Gustavo Duarte <gutorap.gh@gmail.com>
2022-11-09 21:26:50 -0300

    feat: Static (array based) List class

---
+++ src/com/github/geezgus/datastructure/ArrayList.java
@@ -0,0 +1,54 @@
+package com.github.geezgus.datastructure;
+
+@SuppressWarnings("unchecked")
+public class ArrayList<T> extends AbstractArrayCollection<T> implements List<T> {
+
+    public ArrayList() {
+        super();
+    }
+
+    public ArrayList(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public int indexOf(T item) {
+        for (int i = 0; i < size(); i++) {
+            if (item.equals(array[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public void insert(int index, T item) throws IndexOutOfBoundsException {
+        assertIndexIsSettable(index);
+        includeAt(index, item);
+    }
+
+    @Override
+    public T get(int index) throws IndexOutOfBoundsException {
+        assertIndexIsGettable(index);
+        return (T) array[index];
+    }
+
+    @Override
+    public void removeAt(int index) throws IndexOutOfBoundsException {
+        assertIndexIsGettable(index);
+        excludeAt(index);
+    }
+
+    @Override
+    public void set(int index, T item) throws IndexOutOfBoundsException {
+        assertIndexIsGettable(index);
+        array[index] = item;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
